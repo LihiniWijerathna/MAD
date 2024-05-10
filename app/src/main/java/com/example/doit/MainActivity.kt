a2b91fe322a2828818bebceb997eb9f090df11ea
Lihini Wijerathne <IT22891990@my.sliit.lk>
2024-05-10 23:39:00 +0530

    initial commit - layout

---
+++ app/src/main/java/com/example/doit/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.doit
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.doit.databinding.ActivityAddnoteBinding
+import com.example.doit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,Addnote::class.java)
+            startActivity(intent)
+        }
+        }
+    }
---
+++ app/src/main/java/com/example/doit/Note.kt
@@ -0,0 +1,3 @@
+package com.example.doit
+
+data class Note(val id:Int,val title:String,val content:String)
