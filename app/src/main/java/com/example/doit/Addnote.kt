dbd73edf391e87537c87fcec1884dc1609f89576
Lihini Wijerathne <IT22891990@my.sliit.lk>
2024-05-11 23:57:38 +0530

    upto update finish

--- app/src/main/java/com/example/doit/Addnote.kt
+++ app/src/main/java/com/example/doit/Addnote.kt
@@ -2,10 +2,7 @@ package com.example.doit
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.doit.databinding.ActivityAddnoteBinding
 
 class Addnote : AppCompatActivity() {
